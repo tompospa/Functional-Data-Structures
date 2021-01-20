f9dfeb93e8bfcbcde3189d3cadc2d7fa7dbccbd7
tompospa <74552059+tompospa@users.noreply.github.com>
2021-01-20 22:05:48 +0100

    třídy deque

--- Real-time deque/Deque.py
+++ Real-time deque/Deque.py
@@ -1,2 +1,17 @@
 class deque(object):
     pass
+
+    def new():
+        pass
+
+    def is_empty():
+        pass
+
+    def push(v, dq):
+        pass
+
+    def pop(dq):
+        pass
+
+    def length(dq):
+        pass
